4e3074069f004ea7fa6999277724b4668df25051
WrishavSett <wrishavsett2912@gmail.com>
2025-09-06 15:28:27 +0530

    Required files and folders

--- utils/downloadModels.py
+++ utils/downloadModels.py
@@ -1,7 +1,7 @@
 from huggingface_hub import snapshot_download
 
 # Choose your target directories
-dir = "./models/<model-name>"
+dir = "./models/sentence-transformers/all-MiniLM-L6-v2"
 
 # Download full repos into those directories
-snapshot_download(repo_id="llava-hf/<model-name>", local_dir=dir)
+snapshot_download(repo_id="sentence-transformers/all-MiniLM-L6-v2", local_dir=dir)
